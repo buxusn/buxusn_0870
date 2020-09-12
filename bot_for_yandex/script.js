f54f90902588e1af9de7cebe60d28f5b0d974769
buxusn <71059267+buxusn@users.noreply.github.com>
2020-09-12 20:13:26 +0300

    12.09.2020
    Заданный сайт открывается, не переходя при этом на новую вкладку.

--- bot_for_yandex/script.js
+++ bot_for_yandex/script.js
@@ -7,25 +7,35 @@
 // @match        https://yandex.ru/*
 // @grant        none
 // ==/UserScript==
-let keywords = ['Гобой', 'Как звучит флейта', 'Кларнет' ];
+let keywords = ["Гобой", "Как звучит флейта", "Кларнет"];
 let keyword = keywords[getRandom(0, keywords.length)];
 
-let text = document.getElementsByClassName ('input__control input__input mini-suggest__input') [0];
+let text = document.getElementsByClassName(
+  "input__control input__input mini-suggest__input"
+)[0];
 
-if (text !=undefined) {
-    document.getElementsByClassName ('input__control input__input mini-suggest__input') [0].value=keyword;
-    document.getElementsByTagName('button') [0].click();
-}else {
-    let links = document.links;
-    for (let i=0; i<links.length;i++){
-        if (links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
-            console.log ("Ссылка найдена" + links[i]);
-            links[i].click();
-            break;
-        }
+if (text != undefined) {
+  document.getElementsByClassName(
+    "input__control input__input mini-suggest__input"
+  )[0].value = keyword;
+  document.getElementsByTagName("button")[0].click();
+} else {
+  let links = document.links;
+  Array.from(document.querySelectorAll('a[target="_blank"]')).forEach((link) =>
+    link.removeAttribute("target")
+  );
+
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1) {
+      console.log("Ссылка найдена" + links[i]);
+
+      links[i].click();
+
+      break;
     }
+  }
 }
 
-function getRandom(min, max){
-    return Math.floor(Math.random()*(max-min)+min);
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
